b7bb240b49dc057b687a6b36800fcbd4a88bb4cf
Tobias Leong <tobias.leong@zendesk.com>
2022-12-13 12:39:17 +0800

    Add fetch from API endpoint

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -3,5 +3,5 @@ export async function load({fetch}) {
     const res = await fetch('https://api.data.gov.sg/v1/transport/taxi-availability');
     const item = await res.json();
 
-    return {item};
+    return {item}
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,12 @@
     export let data;
     console.log("Data loaded...");
     console.log(data);
-
     const taxiCount = data['item']['features'][0]['properties']['taxi_count'];
+    const timestamp = data['item']['features'][0]['properties']['timestamp'];
 </script>
 
 <h1>Welcome to Taxi Visualiser</h1>
+<p>Data retrieved: {timestamp}</p>
 <p>Taxi count: {taxiCount}</p>
 
 <a href="./about">about</a>
