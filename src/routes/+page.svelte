5fdb6d7b6db62268bf4675ea926f90550ad9affe
Tobias Leong <tobias.leong@zendesk.com>
2022-12-13 13:08:05 +0800

    Add styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,49 @@
     const taxiCount = data['item']['features'][0]['properties']['taxi_count'];
     const timestamp = data['item']['features'][0]['properties']['timestamp'];
 </script>
-
-<h1>Welcome to Taxi Visualiser</h1>
-<p>Data retrieved: {timestamp}</p>
-<p>Taxi count: {taxiCount}</p>
-
-<a href="./about">about</a>
+<div class="main">
+    <div class="title">
+        <h1>Taxi Visualiser</h1>
+    </div>
+    <div class="data-info">
+        <p>Data retrieved: {timestamp}</p>
+        <p>Taxi count: {taxiCount}</p>
+    </div>
+    <a href="./about">about</a>
+</div>
 
 <style>
+    :global(body) {
+        background-color: #000000;
+        font-family:'Courier New', Courier, monospace;
+        color: whitesmoke;
+    }
+
+    .main {
+        display: flex;
+        flex-flow: column wrap;
+        justify-content: space-evenly;
+        flex-grow: 1;
+        align-items: center;
+    }
+
+    .title > h1 {
+        font-weight: bold;
+        font-size: 76px;
+        text-align: center;
+    }
+
+    .data-info {
+        display: flex;
+        flex-flow: row wrap;
+        flex-grow: 1;
+        justify-content: space-around;
+        max-width: 500px;
+        text-align: center;
+        padding: 10px 10px;
+        margin: 10px 10px;
+        gap: 10px;
+    }
     a:hover {
         color:gold;
     }
