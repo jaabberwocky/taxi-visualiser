a30b4be0f44870c5c3b12a4a13a72a15dc23e5ea
Tobias Leong <tobias.leong@zendesk.com>
2022-12-13 08:25:34 +0800

    Initial commit

---
+++ src/routes/+page.js
@@ -0,0 +1,7 @@
+// for loading of data which is then shared with +page
+export async function load({fetch}) {
+    const res = await fetch('https://api.data.gov.sg/v1/transport/taxi-availability');
+    const item = await res.json();
+
+    return {item};
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let data;
+    console.log("Data loaded...");
+    console.log(data);
+
+    const taxiCount = data['item']['features'][0]['properties']['taxi_count'];
+</script>
+
+<h1>Welcome to Taxi Visualiser</h1>
+<p>Taxi count: {taxiCount}</p>
+
+<a href="./about">about</a>
+
+<style>
+    a:hover {
+        color:gold;
+    }
+</style>
